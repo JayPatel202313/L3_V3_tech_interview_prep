920d604badc2cbf19647c08dd4284430148bccee
JayPatel202313 <jpatel@pursuit.org>
2025-04-28 08:59:36 -0400

    Solved 3 problems for week 1 and tested

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -1,16 +1,20 @@
 // Reverse a string
 function reverseString(str) {
-
+    return str.split('').reverse().join('');
 }
   
 // Find max in array
 function findMax(arr) {
-
+    return Math.max(...arr);
 }
 
 // Check if palindrome
 function isPalindrome(word) {
-    
+    for(let i=0; i < Math.floor(word.length/2); i++){
+        if(word[i] != word[word.length-i-1])
+            return false;
+    }
+    return true;
 }
 
 module.exports = { reverseString, findMax, isPalindrome };
